6c3a44bd864422f22420d892962fd664c3171f06
Artem Tkachuk <artemtkachuk@yahoo.com>
2019-09-05 08:03:38 -0700

    Little bit of refactoring

--- python/gaussJordan.py
+++ python/gaussJordan.py
@@ -1,35 +1,24 @@
 import numpy as np
 
 def gaussJordan(a):
-
-    # if not isSquare(a):   #TODO fix
-    #     return False
+    if not isSquare(a):   #TODO fix
+        return False
 
     n = len(a)  #number of rows
 
     for k in range(n):
         first_non_zero_row, first_non_zero_col = findNonZero(a, k, n)
-
         if first_non_zero_col == -1:    #zero matrix
             break
-
         if a[k][first_non_zero_col] == 0:
             a = swapRows(a, n, k, first_non_zero_row)   #TODO scale?, debug?
-
-        first = a[k][first_non_zero_col]
-        for i in range(k, n + 1):
-            a[k][i] = a[k][i] / first
-
-
-        for i in range(k + 1, n):
-            first = a[i][first_non_zero_col]
-            for j in range(k, n + 1):
-                a[i][j] -= a[first_non_zero_row][j] * first
+        a = divideRow(a, n, k, first_non_zero_col)
+        a = subtractRows(a, n, k, first_non_zero_row, first_non_zero_col)
 
     for row in a:
         print(row)
 
-    for k in range(n - 1, 0):
+    # for k in range(n - 1, 0):
 
     #Выбирают первый слева столбец матрицы, в котором есть хоть одно отличное от нуля значение.
     # Если самое верхнее число в этом столбце ноль, то меняют всю первую строку матрицы с другой строкой матрицы, где в этой колонке нет нуля.
@@ -39,6 +28,19 @@ def gaussJordan(a):
     # После повторения этой процедуры n − 1 раз получают верхнюю треугольную матрицу
     # Вычитают из предпоследней строки последнюю строку, умноженную на соответствующий коэффициент, с тем, чтобы в предпоследней строке осталась только 1 на главной диагонали.
     # Повторяют предыдущий шаг для последующих строк. В итоге получают единичную матрицу и решение на месте свободного вектора (с ним необходимо проводить все те же преобразования).
+
+def divideRow(a, n, k, first_non_zero_col):
+    first = a[k][first_non_zero_col]
+    for i in range(k, n + 1):
+        a[k][i] = a[k][i] / first
+    return a
+
+def subtractRows(a, n, k, first_non_zero_row, first_non_zero_col):
+    for i in range(k + 1, n):
+        first = a[i][first_non_zero_col]
+        for j in range(k, n + 1):
+            a[i][j] -= a[first_non_zero_row][j] * first
+    return a
 
 def swapRows(a, n, i, j):
     for k in range(n + 1):
@@ -59,9 +61,9 @@ def isSquare(a):
 
 
 def main():
-    a = [[1, 1, 1, 3],
-         [2, 3, 7, 0],
-         [1, 3, -2, 17]]
+    a = [[2, 1, 3, 0],
+         [1, 2,0, 0],
+         [0, 1, 1, 0]]
     print(gaussJordan(a))
 
 if __name__ == '__main__':
