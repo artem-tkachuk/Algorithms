7ae9ecb9fd12f122b7bd92088a0bd78313003319
Artem Tkachuk <artemtkachuk@yahoo.com>
2023-01-16 15:22:51 -0800

    Break

--- python/freeCodeCamp/dp/canConstruct.py
+++ python/freeCodeCamp/dp/canConstruct.py
@@ -1,35 +1,38 @@
-def canConstruct(target_string, word_bank):
+def canConstructMemoized(target_string, word_bank):
+    return canConstructMemoizedHelper(target_string, word_bank, memo={})
+
+def canConstructMemoizedHelper(target_string, word_bank, memo={}):
+    # have we solved/cached this problem instance before
+    if target_string in memo:
+        return memo[target_string]
     # if we arrived at an empty string, and thus constructed a valid decomposition
     if target_string == "":
         return True
-    # else try all words from the word bank
-    # # start off by saying it is not possible and potentially changing that to True
-    # possible_to_construct = False
     # go through all the words from the bank
     for word in word_bank:
         # only branch out for the ones that are prefixes to the target_string
-        if isPrefix(what=word, of_what=target_string):
+        if isNonEmptyPrefix(what=word, of_what=target_string):
             # length of the prefix we should cut out from the original target_string
             prefix_len = len(word)
             # whatever remains after slicing
-            new_target_string = target_string[prefix_len:]
+            suffix = target_string[prefix_len:]
             # branch out for the new target string
-            if canConstruct(new_target_string, word_bank):
+            if canConstructMemoizedHelper(suffix, word_bank, memo):
                 # and potentially terminate early
+                memo[target_string] = True
                 return True
     # none of the prefixes worked
+    memo[target_string] = False
     return False
 
-def isPrefix(what, of_what):
+def isNonEmptyPrefix(what, of_what):
     prefix_len = len(what)
-    target_string_len = len(of_what)
-    # check that prefix is not empty and is at most as long as target 
-    # to prevent infinite loops and incorrect slicing attempts
-    return (0 < prefix_len <= target_string_len) and of_what.find(what) == 0
+    # check that prefix is not empty to prevent infinite loops
+    return prefix_len > 0 and of_what.find(what) == 0
 
 # Testing
-print(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd", ""])) # true
-print(canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])) # false
-print(canConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])) # true
-print(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", \
+print(canConstructMemoized("abcdef", ["ab", "abc", "cd", "def", "abcd", ""])) # true
+print(canConstructMemoized("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])) # false
+print(canConstructMemoized("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])) # true
+print(canConstructMemoized("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", \
     ["e", "ee", "eee", "eeee", "eeeee", "eeeeee"])) # false
