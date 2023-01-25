c2c6e487533a1c9588f86b432b3e57a3b06b60c6
Artem Tkachuk <artemtkachuk@yahoo.com>
2023-01-24 23:42:33 -0800

    Validate IP address

--- python/leetcode/532KdiffPairsInArray.py
+++ python/leetcode/532KdiffPairsInArray.py
@@ -1,6 +1,36 @@
+from typing import List
+
 class Solution:
     def findPairs(self, nums: List[int], k: int) -> int:
-        pass
+        # number of elements in the array
+        n = len(nums)
+        # sort the nums array
+        nums.sort()
+        # initialize the two pointers
+        l, r = 0, 0
+        # initialize count
+        count = 0
+        # go through the sorted numbers
+        while r < n:
+            if l == r:
+                r += 1
+            else:
+                # what is the current difference?
+                curr_diff = abs(nums[l] - nums[r])
+                
+                if curr_diff == k:
+                    # increment count
+                    count += 1
+                    # update pointers
+                    l += 1
+                    r += 1
+                elif curr_diff < k:
+                    r += 1
+                # curr_diff > k
+                else:
+                    l += 1
+
+        return count
 
 print(Solution().findPairs([3, 1, 4, 1, 5], k=2))
 print(Solution().findPairs([1, 2, 3, 4, 5], k=1))
