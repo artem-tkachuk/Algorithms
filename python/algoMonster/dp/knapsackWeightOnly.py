0a6b6411ec2ed2acb908cf834c69eea6df962734
Artem Tkachuk <artemtkachuk@yahoo.com>
2023-01-28 14:41:30 -0800

    Ugly, but a working solution to knapsack weight-only. Will improve

--- python/algoMonster/dp/knapsackWeightOnly.py
+++ python/algoMonster/dp/knapsackWeightOnly.py
@@ -1,16 +1,22 @@
 from typing import List
 
-def knapsack(weights: List[int], valuesSet) -> List[int]:
-    valuesSet = set()
+def knapsack(weights: List[int]) -> List[int]:
+    return knapsackHelper(weights, valuesSet=set())
 
+def knapsackHelper(weights: List[int], valuesSet: set = set()) -> List[int]:
     n = len(weights)
 
+    if n == 0:
+        return [0]
+
     for i in range(n):
-        valuesSet |= knapsack(weights[:i] + weights[])
-    valuesSet.add()
+        valuesSet |= set(knapsackHelper(weights[:i] + weights[(i + 1):], valuesSet))
+    valuesSet.add(sum(weights))
+
     return list(valuesSet)
     
 
 print(knapsack([1, 3, 3, 5]))
+print(knapsack([1, 2, 3]))
 
 
