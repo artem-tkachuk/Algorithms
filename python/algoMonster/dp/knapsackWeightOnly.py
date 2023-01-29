ee71430a65709fea815812eff41f560d6eb53f32
Artem Tkachuk <artemtkachuk@yahoo.com>
2023-01-28 19:24:55 -0800

    Some comments and code refactoring

--- python/algoMonster/dp/knapsackWeightOnly.py
+++ python/algoMonster/dp/knapsackWeightOnly.py
@@ -1,21 +1,25 @@
 from typing import List
 
 def knapsack(weights: List[int]) -> List[int]:
-    return knapsackHelper(weights, valuesSet=set())
+    return list(knapsackHelper(weights, valuesSet=set()))
 
-def knapsackHelper(weights: List[int], valuesSet: set = set()) -> List[int]:
+def knapsackHelper(weights: List[int], valuesSet: set) -> List[int]:
+    # number of elements in the weights array
     n = len(weights)
-
+    # Base case: 0 elements in weights gives sum of 0
     if n == 0:
-        return [0]
-
-    for i in range(n):
-        valuesSet |= set(knapsackHelper(weights[:i] + weights[(i + 1):], valuesSet))
+        return set([0])
+    # Add the sum of weights itself to the set
     valuesSet.add(sum(weights))
-
-    return list(valuesSet)
+    # go over each element and use it, recursively calling knapsack on remaining elements
+    for i in range(n):
+        weights_without_i = weights[:i] + weights[(i + 1):]
+        # merge all possible sums of the weights array exclusing i'th elem to all the valuesSet
+        valuesSet |= knapsackHelper(weights_without_i, valuesSet)
+    # return all possible sums for the current weights array
+    return valuesSet
     
-
+# Testing
 print(knapsack([1, 3, 3, 5]))
 print(knapsack([1, 2, 3]))
 
